626ba36c697c4bea1f21bd558a95f9f0b062d166
mstilde <matiasemanuelbuttari@hotmail.com>
2025-04-29 11:33:20 -0300

    second commit

--- app/src/main/java/com/example/batallanaval/MainActivity.kt
+++ app/src/main/java/com/example/batallanaval/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.batallanaval
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +13,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    fun seleccionarCasillero(v: View) {
+
+    }
+
+    fun atacar(v: View) {
+
+    }
+
+    fun nuevoJuego(v: View) {
+
+    }
 
 }
