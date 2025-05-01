f971f4bcc0be2a115a30d3f0529ad3a0d230177f
mstilde <matiasemanuelbuttari@hotmail.com>
2025-05-01 15:49:53 -0300

    third commit

--- app/src/main/java/com/example/batallanaval/MainActivity.kt
+++ app/src/main/java/com/example/batallanaval/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.batallanaval
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
+    var intentos = 0;
+    var aciertos = 0;
+    var barcosADerribar = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,7 +30,30 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun nuevoJuego(v: View) {
+        barcosADerribar = Random.nextInt(10, 16);
+        colocarBarcos();
+        var contadorDeBarcos = findViewById<TextView>(R.id.cantidadBarcos);
+        contadorDeBarcos.setText(barcosADerribar.toString());
+    }
+
+    fun colocarBarcos() {
+        var columna = 0;
+        var fila = 0;
+        var coordenada = "";
+
+        for (b in 1..barcosADerribar) {
+            var colocando = true;
+            while (colocando) {
+                columna = Random.nextInt(6) + 1 * 10;
+                fila = Random.nextInt(6) + 1;
+                coordenada = (columna + fila).toString();
+                val idRecurso = resources.getIdentifier(coordenada, "id", packageName);
+                val casillero = findViewById<Button>(idRecurso);
+            }
+
+        }
 
     }
+
 
 }
