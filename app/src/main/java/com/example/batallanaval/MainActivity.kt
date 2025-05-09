7dccff8e08521ab33d5637e04205503ed7f1be7c
mstilde <matiasemanuelbuttari@hotmail.com>
2025-05-09 13:30:59 -0300

    nueva version batalla naval

--- app/src/main/java/com/example/batallanaval/MainActivity.kt
+++ app/src/main/java/com/example/batallanaval/MainActivity.kt
@@ -14,10 +14,19 @@ import kotlin.random.Random
 import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
+    //Variables globales
+
+    // Construir un tablero dentro de la logica kotlin declarando un lateinit para inicializarlo en el onCreate usando un layout TableLayout
     lateinit var tablero: TableLayout
-    val filas = listOf("A", "B", "C", "D", "E", "F")
+    // Dimensiones del tablero.
+    val filas = arrayOf("A", "B", "C", "D", "E", "F")
     val columnas = 6
-    lateinit var botones: Array<Array<Button>>
+    // Casilleros de tablero en una matriz 6x6
+    lateinit var casilleros: Array<Array<Button>>
+    // Usamos con "var" una variable para poder guardar el casillero seleccionado. Usamos un null como valor inicial ya que al iniciar el juego no tiene valor seleccionado.
+    var casilleroSeleccionado : Button? = null;
+
+    // Almacenamiento interno de las estadisticas.
     var intentos = 0;
     var aciertos = 0;
     var barcosADerribar = 0;
@@ -26,56 +35,74 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Tomamos en layout del xml por el id y lo almacenamos en la variable. Luego generamos el tablero.
         tablero = findViewById(R.id.tablero)
         generarTablero()
     }
 
     fun generarTablero() {
-        botones = Array(filas.size) { row ->
-            Array(columnas) { col ->
-                val boton = Button(this).apply {
-                    text = "${filas[row]}${col+1}"
-                    layoutParams = TableRow.LayoutParams(0, 70).apply {
+        // Con removeAllViews limpiamos los casilleros con todas las configuraciones previas en caso de estar iniciado el juego de nuevo
+        tablero.removeAllViews()
+        // Se genera el tablero limpio de 0
+        casilleros = Array(filas.size) { fil -> // Creamos un array para cada fila (fil) según el tamaño del array (6) y lo almacenamos en la matriz "casilleros"
+            Array(columnas) { col -> // Hacemos lo mismo creando un array para cada columna. Cada valor col indicará el número de la columna.
+                val boton = Button(this).apply { // En cada coordenada (fila y columna) de la matriz, creamos un botón en el contexto "this" y le aplicamos los sigientes datos:
+                    text = "${filas[fil]}${col+1}" // Como texto, el valor actual de la letra de la fila y el de la columna + 1 (ya que los indices del array de dimension 6, van del 0 al 5)
+                    layoutParams = TableRow.LayoutParams(0, 70).apply { // Agregar dimensiones de ancho, alto y aplicar weight para que los botones ocupen el mismo espacio dentro de la fila
                         weight = 1f
                     }
-                    tag = "agua"
-                    setOnClickListener { seleccionarCasillero(it) }
+                    tag = "agua" //Agregarle un tag para identificar de que casillero se trata, si de agua o barco. En este caso como inicializa de 0 el tablero, se coloca en "agua"
+                    setOnClickListener { seleccionarCasillero(this) } // Le damos también un listener para que al clickarlo se active la función seleccionarCasillero
                 }
-                boton
+                boton // Acá se devuelve la variable que creamos recién para almacenarlo en el array de la columna
             }
         }
 
-        for (fila in botones) {
-            val tableRow = TableRow(this)
-            fila.forEach { boton -> tableRow.addView(boton) }
-            tablero.addView(tableRow)
+        // Acá agregamos la estructura de matriz generada anteriormente a la interfaz de usuario
+        for (fila in casilleros) { // Hacemos un for por cada fila en la tabla de casilleros
+            val tableRow = TableRow(this) // Creamos una variable TableRow en donde almacenaremos los botones
+            fila.forEach { boton -> tableRow.addView(boton) } // Con el forEach vamos cada botón en el tableRow que creamos para la interfaz con addView
+            tablero.addView(tableRow) // Finalmente, al TableLayout tablero vamos agregandole su respectiva fila para mostrar en la interfaz
         }
     }
 
-    fun seleccionarCasillero(v: View) {
-        val boton = v as Button
+    fun seleccionarCasillero(boton: Button) {
+        casilleroSeleccionado?.setBackgroundResource(android.R.drawable.btn_default)
+
+        casilleroSeleccionado = boton
+        boton.setBackgroundColor(resources.getColor(android.R.color.holo_blue_light, theme))
+    }
+
+    fun atacar(v: View) {
+        val boton = casilleroSeleccionado ?: return
+
+        if (boton.text == "X" || boton.text == "O") return
+
         if (boton.tag == "barco") {
             boton.text = "X"
             aciertos++
         } else {
             boton.text = "O"
         }
+
         intentos++
         actualizarContadores()
-    }
 
-    fun atacar(v: View) {
-
+        boton.setBackgroundResource(android.R.drawable.btn_default)
+        casilleroSeleccionado = null
     }
 
     fun nuevoJuego(v: View) {
-        barcosADerribar = Random.nextInt(10, 16);
-        colocarBarcos();
-        findViewById<TextView>(R.id.cantidadBarcos).text = barcosADerribar.toString()
         intentos = 0
         aciertos = 0
-        actualizarContadores()
+        barcosADerribar = Random.nextInt(10, 16);
+
         generarTablero()
+        colocarBarcos();
+
+        findViewById<TextView>(R.id.cantidadBarcos).text = barcosADerribar.toString()
+        actualizarContadores()
+
     }
 
     fun colocarBarcos() {
@@ -83,9 +110,9 @@ class MainActivity : AppCompatActivity() {
         while (colocados < barcosADerribar) {
             val fila = Random.nextInt(filas.size)
             val col = Random.nextInt(columnas)
-            val boton = botones[fila][col]
+            val boton = casilleros[fila][col]
             if (boton.tag == "agua") {
-                boton.tag == "barco"
+                boton.tag = "barco"
                 colocados++
             }
         }
